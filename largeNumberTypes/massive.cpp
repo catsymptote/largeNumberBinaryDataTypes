9f4cdf3b7005c44472dafeb1cd75f600ae0c5980
catsymptote <catsymptote@gmail.com>
2018-05-03 00:34:25 +0200

    Set, store, and binary print implemented.

---
+++ largeNumberTypes/massive.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+#include "massive.h"
+
+#include <iostream>		// For printing
+#include <string>		// For printing
+#include <cmath>		// For powers etc
+
+massive::massive() {}
+
+massive::~massive() {}
+
+void massive::setNumber(long long int num)
+{
+	unsigned int exponent = 0;
+	while (std::pow(2, exponent) <= num)
+	{
+		exponent++;
+	}
+
+	/// Fill vector with needed amonuts of 0's
+	this->size = exponent;
+	this->number.assign(exponent, 0);
+
+	/// Loop through number
+	unsigned int pwr;
+	for(int i = exponent - 1; i >= 0; i--)
+	{
+		pwr = (unsigned int)(std::pow(2, i));
+		if (num >= pwr)
+		{
+			this->number.at(i) = 1;
+			num -= pwr;
+			std::cout << num << std::endl;
+		}
+	}
+}
+
+std::vector<bool> massive::getBinary()
+{
+	return this->number;
+}
+
+void massive::binaryPrint()
+{
+	std::string numAsText = "";
+	for (int index = this->size - 1; index >= 0; index--)
+	{
+		if (this->number.at(index))
+			numAsText += "1";
+		else
+			numAsText += "0";
+	}
+	std::cout << numAsText << std::endl;
+}
---
+++ largeNumberTypes/massive.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+
+class massive
+{
+public:
+	massive();
+	~massive();
+
+private:
+	std::vector<bool> number;
+	unsigned int size;
+
+public:
+	void setNumber(long long int num);
+	std::vector<bool> getBinary();
+	long long int getDecimal();
+
+	unsigned int getSize();
+
+	void binaryPrint();
+
+	
+
+
+	void add();
+	void sub();
+	void mul();
+	void div();
+	void pow();
+	void increment();
+
+};
